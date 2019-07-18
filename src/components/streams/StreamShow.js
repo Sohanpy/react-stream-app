0d56c18fa8a56f6695b1c6f8e87fe9c1ccfc6b53
Shahriar Sohan <sohanmock@gmail.com>
2019-07-18 15:31:31 +0600

    finishing touch

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
-const StreamShow = () => {
-    return (
-        <div>StreamShow</div>
-    )
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { singleStreams } from '../../action';
+class StreamShow extends Component {
+    componentDidMount() {
+        this.props.singleStreams(this.props.match.params.id);
+    }
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        return (
+            <div>
+                <h1>{this.props.stream.title}</h1>
+                <h5>{this.props.stream.description}</h5>
+            </div>
+        )
+    }
 }
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.stream[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { singleStreams })(StreamShow);
